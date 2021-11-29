3d2b5f5b39b7c98363fa064107a8cde27e381187
Konstantin Biriukov <kostya-leon@mail.ru>
2021-11-29 14:52:54 -0600

    init

---
+++ vue-table/src/components/VueTable.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table v-if="posts.length"
+                  :headers="headers"
+                  :items="posts"
+                  :items-per-page="10"
+                  :search="search"
+                  class="elevation-1">
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'VueTable',
+  data() {
+    return {
+      search: ""
+    }
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts;
+    },
+    headers() {
+      return this.$store.state.headers;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
